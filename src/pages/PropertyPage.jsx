f1d51326415520f6d3b5d63deb09ed15bd20c97a
Theran27 <duhulu.20231728@iit.ac.lk>
2025-12-31 03:19:43 +0530

    Finalise property details page layout

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import properties from "../data/properties.json";
 import PropertyGallery from "../components/property/PropertyGallery";
+import PropertyTabs from "../components/property/PropertyTabs";
 
 const PropertyPage = () => {
     const { id } = useParams();
 
     const property = properties.find(
-        (p) => p.id === Number(id)
+        (item) => item.id === Number(id)
     );
 
     if (!property) {
@@ -14,14 +15,19 @@ const PropertyPage = () => {
     }
 
     return (
-        <div>
+        <div style={{ padding: "20px" }}>
+            {/* Title & summary */}
             <h2>{property.shortDescription}</h2>
             <h3>£{property.price.toLocaleString()}</h3>
             <p>
                 {property.bedrooms} bedrooms • {property.postcode}
             </p>
 
+            {/* Image gallery */}
             <PropertyGallery images={property.images} />
+
+            {/* Tabs: Description / Floor Plan / Map */}
+            <PropertyTabs property={property} />
         </div>
     );
 };
