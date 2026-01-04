736a42074686780757e8284317634a68a8ebab5c
Theran27 <duhulu.20231728@iit.ac.lk>
2026-01-05 03:49:36 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line no-undef
+  base:process.env.VITE_BASE_PATH || "/estate-agent-app-deploy",
   test: {
     globals: true,
     environment: "jsdom",
